5dc282c1d2629b6795d891a5f64f7c441613b706
bar21457 <bar21457@uvg.edu.gt>
2022-08-15 10:21:36 -0600

    Formato
    Se colocó el formato de la estructura del código

--- Lab3.X/main.s
+++ Lab3.X/main.s
@@ -1,3 +1,77 @@
+;*******************************************************************************
+; Universidad del Valle de Guatemala
+; IE20203 Programación de Microcontroladores
+; Autor: Byron Barrientos
+; Compilador: PIC-AS (v2.36), MPLAB X IDE (v.600)
+; Proyecto: TMR0_y_Botones
+; Hardware: PIC16F887
+; Creado: 09/08/2022
+; Última Modificación: 15/08/2022
+;*******************************************************************************
 
+PROCESSOR 16F887
+#include <xc.inc>
+;*******************************************************************************
+;Palabra de configuración
+;***************************************************************************
+; CONFIG1
+  CONFIG  FOSC = INTRC_NOCLKOUT ; Oscillator Selection bits (INTOSCIO 
+                                ;oscillator: I/O function on RA6/OSC2/CLKOUT
+				;pin, I/O function on RA7/OSC1/CLKIN)
+  CONFIG  WDTE = OFF             ; Watchdog Timer Enable bit (WDT enabled)
+  CONFIG  PWRTE = OFF           ; Power-up Timer Enable bit (PWRT disabled)
+  CONFIG  MCLRE = OFF           ; RE3/MCLR pin function select bit (RE3/MCLR 
+                                ;pin function is digital input, MCLR internally
+				;tied to VDD)
+  CONFIG  CP = OFF              ; Code Protection bit (Program memory code
+                                ;protection is disabled)
+  CONFIG  CPD = OFF             ; Data Code Protection bit (Data memory code
+                                ;protection is disabled)
+  CONFIG  BOREN = OFF           ; Brown Out Reset Selection bits (BOR disabled)
+  CONFIG  IESO = OFF            ; Internal External Switchover bit 
+                                ;(Internal/External Switchover mode is disabled)
+  CONFIG  FCMEN = OFF           ; Fail-Safe Clock Monitor Enabled bit 
+                                ;(Fail-Safe Clock Monitor is disabled)
+  CONFIG  LVP = OFF             ; Low Voltage Programming Enable bit (RB3 pin
+                                ;has digital I/O, HV on MCLR must be used for 
+				;programming)
 
+; CONFIG2
+  CONFIG  BOR4V = BOR40V        ; Brown-out Reset Selection bit (Brown-out 
+                                ;Reset set to 4.0V)
+  CONFIG  WRT = OFF             ; Flash Program Memory Self Write Enable bits 
+                                ;(Write protection off)
 
+;*******************************************************************************
+;Variables
+;*******************************************************************************
+PSECT udata_shr
+
+ 
+;*******************************************************************************
+;Vector Reset
+;*******************************************************************************
+PSECT CODE, delta=2, abs
+ ORG 0x0000
+    GOTO MAIN
+
+;*******************************************************************************
+;Código Principal
+;*******************************************************************************
+PSECT CODE, delta=2, abs
+ ORG 100h
+
+MAIN:
+    
+    
+;*******************************************************************************
+; Ejecución del programa principal
+;*******************************************************************************
+    
+LOOP:
+
+    
+;*******************************************************************************
+; Fin de Código
+;*******************************************************************************
+END
